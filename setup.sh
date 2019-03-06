374d75498b4be809540c8dea0cb7361c81d90caf
Daniel Mancilla <daniel.mancilla@appdynamics.com>
2019-03-05 21:23:05 -0600

    Added ansible role

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PKG_MGR=""
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script as sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+ if [[ ! -z $(which yum) ]]; then
+    PKG_MGR=$(which yum)
+ elif [[ ! -z $(which apt-get) ]]; then
+    PKG_MGR=$(which apt-get)
+ else
+    echo "Neither yum or apt-get are installed"
+    exit 1;
+ fi
+
+PACKAGES=(ansible curl git)
+
+$PKG_MGR install -y ${PACKAGES[*]}
+
